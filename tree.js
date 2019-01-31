8cf9e7a8e95702916b83da8af4f3c7d4c9a8af9c
Tingbin Huang <tingbinhuang@Tingbins-MacBook-Pro.local>
2019-01-31 09:52:41 -0800

    Complete

--- tree.js
+++ tree.js
@@ -1,87 +1,48 @@
-var BinarySearchTree = function (value) {
-	var newTree = {};
-	newTree.value = value;
-	newTree.left = null;
-	newTree.right = null;
-	_.extend(newTree, methods);
-	return newTree;
+
+const addDepth = function (obj, tree) {
+	if (!obj[tree.depth]) {
+		obj[tree.depth] = tree.tree.value;
+	}
+	else {
+		var n = obj[tree.depth];
+		obj[tree.depth] += tree.tree.value;
+	}
+}
+
+const findLargestLevel = function (node) {
+	// your code here
+
+	if (node === undefined) {
+		return 0;
+	}
+	let maximum = 0;
+	let obj = { tree: node, depth: 0 };
+	let queue = [obj];
+	let list = {};
+	let sum = node.value;
+
+	while (queue.length !== 0) {
+
+		let root = queue.shift();
+
+		if (root.left !== undefined) {
+			let tmp = { tree: root.left, depth: root.depth + 1 };
+			addDepth(obj, tmp);
+			queue.push(root.left);
+		}
+
+		if (root.right !== undefined) {
+			let tmp = { tree: root.right, depth: root.depth + 1 };
+			addDepth(obj, tmp);
+			queue.push(root.right);
+		}
+	}
+
+	for (let key in obj) {
+		if (obj[key] > maximum) {
+			maximum = obj[key];
+		}
+	}
+	return maximum;
+
 };
-
-var methods = {};
-
-methods.insert = function (value) {
-	function addNode(value, root) {
-		if (root === null) {
-			return BinarySearchTree(value);
-		}
-		else if (root.value > value) {
-			root.left = addNode(value, root.left);
-		}
-		else {
-			root.right = addNode(value, root.right);
-		}
-		return root;
-	}
-	addNode(value, this);
-}
-
-methods.contains = function (value) {
-	var list = [-1];
-	function search(value, root) {
-		if (root.value === value) {
-			list[0] = value;
-			return;
-		}
-		if (root.left === null && root.right === null) {
-			return;
-		}
-		else if (root.value > value) {
-			search(value, root.left);
-		}
-		else {
-			search(value, root.right);
-		}
-	}
-	search(value, this);      // call 'search()' function
-	return list[0] === value;
-}
-
-methods.depthFirstLog = function (func) {
-	var list = [];
-	list.push(this.value);
-	func(this.value);
-
-	function dfs(func, root) {
-		if (root === null) {
-			return;
-		}
-		dfs(func, root.left);
-		if (!list.includes(root.value)) {
-			func(root.value);
-			list.push(root.value);
-		}
-		dfs(func, root.right);
-	}
-	dfs(func, this);
-}
-
-//Add a function to printout node in postoder
-methods.inOrderTraversal = function (func) {
-	var list = [];
-	list.push(this.value);
-	func(this.value);
-
-	function dfs(func, root) {
-		if (root === null) {
-			return;
-		}
-		dfs(func, root.left);
-
-		dfs(func, root.right);
-		if (!list.includes(root.value)) {
-			func(root.value);
-			list.push(root.value);
-		}
-	}
-	dfs(func, this);
-}
